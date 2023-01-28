aeb38494ab1257ea486543971b16c723d67db20e
Rajesh Pandian <rajesh_pandian@outlook.com>
2023-01-28 16:32:50 +0000

    deploy terraform changes

--- scripts.ps1
+++ scripts.ps1
@@ -1,8 +1,22 @@
 az ad sp create-for-rbac --name "myML" --role contributor --scopes /subscriptions/35b269d2-7c5c-4f36-abf1-56099fd97991/resourceGroups/test-resource-group --sdk-auth
 
+az ad sp create-for-rbac --name "myML" --role contributor --scopes /subscriptions/35b269d2-7c5c-4f36-abf1-56099fd97991 --sdk-auth
 {
   "clientId": "cc5ba836-cd1a-4bbe-b810-e04de2ab586d",
   "clientSecret": "tct8Q~oqtvig5yV5cqfy1v2ee92LmORzLO7MZayA",
+  "subscriptionId": "35b269d2-7c5c-4f36-abf1-56099fd97991",
+  "tenantId": "1df39095-a0f4-4b9f-abcf-e96d3abd9282",
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  "resourceManagerEndpointUrl": "https://management.azure.com/",
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  "galleryEndpointUrl": "https://gallery.azure.com/",
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
+
+{
+  "clientId": "cc5ba836-cd1a-4bbe-b810-e04de2ab586d",
+  "clientSecret": "xfh8Q~~_PynDfemgKkWTkXhYCMkitjjTu.oyJc08",
   "subscriptionId": "35b269d2-7c5c-4f36-abf1-56099fd97991",
   "tenantId": "1df39095-a0f4-4b9f-abcf-e96d3abd9282",
   "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
