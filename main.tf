5e82166e8ebaa9806b63d5de14eb71cd70ed7129
Rajesh Pandian <rajesh_pandian@outlook.com>
2023-01-28 16:35:03 +0000

    update resource group name

--- main.tf
+++ main.tf
@@ -1,16 +1,10 @@
-resource "random_string" "random" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
-}
-
 resource "azurerm_resource_group" "terraform_resource_group" {
-  name     = random_string.random
+  name     = "resourcegroup0434"
   location = var.location
 }
 
 resource "azurerm_storage_account" "terraform_storage_account" {
-  name                = "storageterraform0914"
+  name                = "storageterraform0434"
   resource_group_name = azurerm_resource_group.terraform_resource_group.name
   location                 = azurerm_resource_group.terraform_resource_group.location
   account_tier             = "Standard"
