bbae1d7e6f9a54666a7d8dd22078c390ca3bac7e
Rajesh Pandian <rajesh_pandian@outlook.com>
2023-02-11 04:23:53 +0000

    deploy app service

--- main.tf
+++ main.tf
@@ -1,16 +1,20 @@
-resource "azurerm_resource_group" "static_website_rg" {
-  name     = "static-website-rg"
+resource "azurerm_resource_group" "terraform_app_service_rg" {
+  name     = "terraform-app-service-rg"
   location = var.location
 }
 
-resource "azurerm_storage_account" "static_website_storage" {
-  name                     = "staticstorage0241"
-  resource_group_name      = azurerm_resource_group.static_website_rg.name
-  location                 = azurerm_resource_group.static_website_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  static_website {
-    index_document = "index.html"
-  }
+resource "azurerm_service_plan" "terraform_app_service_plan" {
+  name                = "terraform-app-service-plan"
+  location            = azurerm_resource_group.terraform_app_service_rg.location
+  resource_group_name = azurerm_resource_group.terraform_app_service_rg.name
+  os_type             = "Windows"
+  sku_name            = "S1"
+}
+
+resource "azurerm_windows_web_app" "terraform_app_service" {
+  name                = "app-service-0420"
+  location            = azurerm_resource_group.terraform_app_service_rg.location
+  resource_group_name = azurerm_resource_group.terraform_app_service_rg.name
+  service_plan_id     = azurerm_service_plan.terraform_app_service_plan.id
+  site_config {}
 }
